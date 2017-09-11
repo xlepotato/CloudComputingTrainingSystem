10a215100519ddf7bd97b36552bdf85daf7af5d9
aloylim98 <31604718+aloylim98@users.noreply.github.com>
2017-09-11 14:50:34 +0800

    sql updated(Populated table)

---
+++ src/entity/Questions.java
@@ -0,0 +1,29 @@
+package entity;
+/**
+ * Created by Aloysius on 11/9/2017.
+ */
+public class Questions {
+    private int questionNo;
+    private String questionDetail;
+    private String answer;
+    private int exerciseNo;
+
+    public Questions(int questionNo, String questionDetail, String answer, int exerciseNo){
+        this.questionNo = questionNo;
+        this.questionDetail = questionDetail;
+        this.answer = answer;
+        this.exerciseNo = exerciseNo;
+    }
+
+    //GET Methods for variables
+    public int getquestionNo(){return questionNo;}
+    public String getquestionDetails(){return questionDetail;}
+    public String getanswer(){return answer;}
+    public int getexerciseNo(){return exerciseNo;}
+
+    //SET Methods for variables
+    public void setquestionNo(int questionNo){this.questionNo = questionNo;}
+    public void setquestionDetails(String questionDetails){this.questionDetail = questionDetails;}
+    public void setanswer(String answer){this.answer = answer;}
+    public void setexerciseNo(int exerciseNo){this.exerciseNo = exerciseNo;}
+}
