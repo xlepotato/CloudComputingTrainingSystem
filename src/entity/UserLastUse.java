1a848aa66d11c5d491e5032ea5eaa33bc46767ce
aloylim98 <31604718+aloylim98@users.noreply.github.com>
2017-09-11 14:07:35 +0800

    sql updated(Populated table)

---
+++ src/entity/UserLastUse.java
@@ -0,0 +1,35 @@
+package entity;
+
+public class UserLastUse {
+
+    private String userId;
+    private int userlevel;
+    private String progress;
+    private String lastBrowse;
+    private String lastLogin;
+
+    public UserLastUse(String userid, int userlevel, String progress, String lastBrowse, String lastlogin)
+    {
+        this.userId = userid;
+        this.userlevel = userlevel;
+        this.progress = progress;
+        this.lastBrowse = lastBrowse;
+        this.lastLogin = lastlogin;
+    }
+
+    //GET Methods for variables
+    public String getuserId() {return userId;}
+    public int getuserLevel() {return userlevel;}
+    public String getprogress() {return progress;}
+    public String getlastBrowse() {return lastBrowse;}
+    public String getlastLogin() {return lastLogin;}
+
+    //SET Methods for variables
+    public void setuserId(String userId) {this.userId = userId;}
+    public void setuserLevel(int userLevel){this.userlevel = userLevel;}
+    public void setprogress(String progress){this.progress = progress;}
+    public void setlastBrowse(String lastBrowse){this.lastBrowse = lastBrowse;}
+    public void setlastLogin(String lastLogin){this.lastLogin = lastLogin;}
+
+
+}
