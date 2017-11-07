b4f6d148fa5b1324c92ab121163ec4f1fd16e6aa
xlepotato <yucia181@gmail.com>
2017-11-08 01:55:37 +0800

    wip password change

--- src/servlet/ChangePasswordServlet.java
+++ src/servlet/ChangePasswordServlet.java
@@ -1,10 +1,13 @@
 package servlet;
 
+import dataManager.UserDAO;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -14,6 +17,16 @@ import java.io.IOException;
 public class ChangePasswordServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        HttpSession session = request.getSession();
+        UserDAO user = new UserDAO();
+       String password = request.getParameter("oldPassword");
+       String newPassword = request.getParameter("newPassword");
+       String confirmNewPassword = request.getParameter("cfmNewPassword");
+
+       if (confirmNewPassword.equals(password)){
+           user.updatePassword(newPassword,user.retrieveUserByUsername(session.getAttribute("username").toString()).getUserId());
+       }else
+           System.out.println("Change password failed");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
