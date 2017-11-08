aa05863e7620de3fd3e5b904b6ca869e1b286a3a
xlepotato <yucia181@gmail.com>
2017-11-08 23:33:53 +0800

    admin change password wip

--- src/servlet/AdminChangePasswordServlet.java
+++ src/servlet/AdminChangePasswordServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Ying on 8/11/2017.
@@ -17,6 +18,7 @@ import java.io.IOException;
 public class AdminChangePasswordServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        PrintWriter pw = response.getWriter();
         HttpSession session = request.getSession();
         UserDAO user = new UserDAO();
         System.out.println("adminchangepw");
@@ -25,6 +27,14 @@ public class AdminChangePasswordServlet extends HttpServlet {
 
         if(password.equals(confirmPassword)){
             user.updatePassword(password, user.retrieveUserByUsername(session.getAttribute("selectedUser").toString()).getUserId());
+            pw.println("<script type=\"text/javascript\">");
+            pw.println("alert('Password has been changed successfully')");
+            if (user.retrieveUserByUsername(session.getAttribute("selectedUser").toString()).getUserType()==0){
+                pw.println("location='adminStudentRecord.jsp';");
+            }else if (user.retrieveUserByUsername(session.getAttribute("selectedUser").toString()).getUserType()==0){
+                pw.println("location='adminTeacherRecord.jsp';");
+            }
+            pw.println("</script>");
         }
 
     }
