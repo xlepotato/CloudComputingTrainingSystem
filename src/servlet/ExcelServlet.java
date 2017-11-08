dc3b7e16326254d0e75d86eff9034c603d457c36
xlepotato <yucia181@gmail.com>
2017-11-08 14:24:27 +0800

    testing

--- src/servlet/ExcelServlet.java
+++ src/servlet/ExcelServlet.java
@@ -14,11 +14,11 @@ import java.io.IOException;
 public class ExcelServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        System.out.println("hi");
-        response.sendRedirect("index.jsp");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        System.out.println("hi");
+        response.sendRedirect("index.jsp");
     }
 }
--- web/adminHome.jsp
+++ web/adminHome.jsp
@@ -155,7 +155,7 @@
                 <div class="button-right-right">
                     <input name="批量导入账号" value="批量导入账号" type="button" style="height:40px;width:100%;"/>
                     <%--<a href="<%=UserUtility.excel()%>">下载模板</a>--%>
-                    <a   href="#" onclick= "<%=response.sendRedirect("/excel")%> return false;" > <i class="glyphicon glyphicon-menu-right icon"  ></i>下载模板</a>
+                    <a href="/excel">下载模板</a>
                 </div>
             </div>
         </div>
